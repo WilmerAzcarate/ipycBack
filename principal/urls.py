f71b16c18cee716d1652ff7285a23e8d6b330c51
Jhon Wilmer Azcarate Rivera <unavidahonorable@hotmail.com>
2023-04-30 14:11:16 -0500

    se acabo el crud de la tabla pais, falta revisar el boton de eliminar debido a que al eliminar te redirecciona y no deberia hacerlo

--- principal/urls.py
+++ principal/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from principal.views import *
 
 urlpatterns = [
-    path('pais/', ListadoPais.as_view(template_name = "pais/tables.html"), name='leerPais')
+    
+    #rutas para la tabla pais
+    #ruta para ver la tabla de paises
+    path('pais/', ListadoPais.as_view(template_name = "pais/index.html"), name='tablaPais'),
+    # La ruta 'detalles' en donde mostraremos una página con los detalles de un Categoria o registro 
+    path('pais/detalle/<int:pk>', PaisDetalle.as_view(template_name = "pais/detalle.html"), name='detallePais'),
+    # La ruta 'actualizar' en donde mostraremos un formulario para actualizar un categoriao registro de la Base de Datos 
+    path('pais/editar/<int:pk>', PaisActualizar.as_view(template_name = "pais/actualizar.html"), name='actualizarPais'), 
+    # La ruta 'crear' en donde mostraremos un formulario para crear un nuevo Categoria o registro  
+    path('pais/crear', PaisCrear.as_view(template_name = "pais/crear.html"), name='crearPais'),
+    # La ruta 'eliminar' que usaremos para eliminar un Categoria o registro de la Base de Datos 
+    path('pais/eliminar/<int:pk>', PaisEliminar.as_view(), name='pais/eliminar.html')    
+
 ]
--- principal/views.py
+++ principal/views.py
@@ -23,6 +23,42 @@ from django import forms
 def Home(request):
     return render(request,'index.html')
 
+#vista paises
 #configuracion para ver la lista de paises
 class ListadoPais(ListView):
     model = Pais
+
+#configuracion para crear un pais
+class PaisCrear(SuccessMessageMixin, CreateView):
+    model = Pais
+    form = Pais
+    fields = "__all__"
+    success_message ='Pais creado correctamente'
+     
+    def get_success_url(self):        
+        return reverse('tablaPais') # Redireccionamos a la vista principal 'leer'
+
+#vista para ver un pais en particular
+class PaisDetalle (DetailView):
+    model = Pais
+
+#vista para actualizar un pais
+class  PaisActualizar(SuccessMessageMixin,UpdateView):
+    model =  Pais
+    form = Pais
+    fields = "__all__" # Le decimos a Django que muestre todos los campos de la tabla 'postres' de nuestra Base de Datos 
+    success_message = 'Pais Actualizado Correctamente !' # Mostramos este Mensaje luego de Editar un Postre 
+
+    def get_success_url(self):               
+        return reverse('tablaPais') # Redireccionamos a la vista principal 'leer'
+
+
+#configuracion para eliminar un pais
+class PaisEliminar(SuccessMessageMixin, DeleteView): 
+    model = Pais 
+    form = Pais
+    fields = "__all__"     
+ 
+    # Redireccionamos a la página principal luego de eliminar un registro o postre
+    def get_success_url(self): 
+        success_message = 'Pais Eliminado Correctamente !' # Mostramos este Mensaje luego de Editar un Postre
