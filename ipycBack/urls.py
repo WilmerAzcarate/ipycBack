6f815653544a381f260526569ef7539f6d5107dd
Jhon Wilmer Azcarate Rivera <unavidahonorable@hotmail.com>
2023-05-12 22:22:03 -0500

    creamos el login y el logout

--- ipycBack/urls.py
+++ ipycBack/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#Login view y logoutView permiten hacer login
+from django.contrib.auth.views import LoginView,LogoutView
 from django.contrib import admin
 from django.urls import path,include
 from principal.views import *
@@ -21,6 +23,8 @@ from principal import urls as principal_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',Home,name='index'),
+    path('home/',Home,name='index'),
+    path('',LoginView.as_view(template_name='login.html'),name='login'),
+    path('logout/',LogoutView.as_view(template_name='login.html'),name='logout'),
     path('principal/',include(principal_urls))
 ]
--- principal/views.py
+++ principal/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 #pantalla principal
 def Home(request):
     return render(request,'index.html')
+
+#pantalla de login
+def Login(request):
+    return render(request,'login.html')
+
+#pantalla de registro
+def Register(request):
+    return render(request,'register.html')
